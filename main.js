59b7524471b48c2437bdbbfdfea9d0c203736624
germano aquino <germano.r.aquino@gmail.com>
2026-01-27 15:55:48 -0300

    feat: collecting info of every store

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
 import headers from "./headers.js";
 import he from "he";
+import setCookieParser from "set-cookie-parser";
 
 const urlParceiras =
   "https://www.trinks.com/Backoffice/Comissao/ExibirProfissionaisRelatorioComissoes";
 const urlServicos =
   "https://www.trinks.com/BackOffice/RankingDeServicos/ObterRankingDeServicos";
 
-const [startDate, finalDate, finalDateUs] = createDateInterval();
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+const finalDay = String(currentDate.getDate() - 1).padStart(2, "0");
+const startDay = "01";
+const startDate = `${startDay}/${month}/${year}`;
+const finalDate = `${finalDay}/${month}/${year}`;
+
+const lojaIds = {
+  14: {
+    idRelacaoProfissional: "46810",
+    idEstabelecimento: "18769",
+  },
+  batista: {
+    idRelacaoProfissional: "103890",
+    idEstabelecimento: "35295",
+  },
+  duque: {
+    idRelacaoProfissional: "440885",
+    idEstabelecimento: "120037",
+  },
+  umarizal: {
+    idRelacaoProfissional: "49102",
+    idEstabelecimento: "19357",
+  },
+};
 
 const data = {
   14: {
@@ -14,79 +40,97 @@ const data = {
     id: [],
     resultados: [],
   },
+  batista: {
+    nome: [],
+    id: [],
+    resultados: [],
+  },
+  duque: {
+    nome: [],
+    id: [],
+    resultados: [],
+  },
+  umarizal: {
+    nome: [],
+    id: [],
+    resultados: [],
+  },
 };
 
 await getPartnersList();
-await getPartnersInfo(data["14"].id);
-console.log(data["14"].nome);
-console.log(data["14"].resultados);
-
-function createDateInterval() {
-  const currentDate = new Date();
-  const year = currentDate.getFullYear();
-  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
-  const finalDay = String(currentDate.getDate() - 1).padStart(2, "0");
-  const startDay = "01";
-  const startDate = `${startDay}/${month}/${year}`;
-  const finalDate = `${finalDay}/${month}/${year}`;
-  const finalDateUs = `${month}/${finalDay}/${year}`;
-
-  return [startDate, finalDate, finalDateUs];
+for (const store in data) {
+  await getPartnersInfo(store);
+  for (const index in data[store].nome) {
+    console.log(data[store].nome[index] + ": ", data[store].resultados[index]);
+  }
+  console.log("\n\n");
 }
 
 async function getPartnersList() {
-  console.log("Pegando lista de parceiras");
+  for (const store in lojaIds) {
+    console.log("Pegando lista de parceiras da loja ", store);
 
-  const requestBody = {
-    TipoData: 1,
-    DataInicio: startDate,
-    DataFim: finalDate,
-    TipoItemPago: 0,
-    ExibirEstornos: false,
-    TipoStatusFiltroPagamento: 1,
-    IdRelacaoProfissional: 46810,
-    mes: 1,
-    ano: 2026,
-    profissional: undefined,
-    indexLinha: 0,
-  };
+    const requestBody = {
+      TipoData: 2,
+      DataInicio: startDate,
+      DataFim: finalDate,
+      TipoItemPago: 0,
+      ExibirEstornos: false,
+      TipoStatusFiltroPagamento: 1,
+      IdRelacaoProfissional: lojaIds[store].idRelacaoProfissional,
+      mes: Number(month),
+      ano: year,
+      profissional: undefined,
+      indexLinha: 0,
+    };
 
-  const encodedBody = new URLSearchParams(requestBody);
+    const idEstabelecimentoPattern = new RegExp(
+      "(?<=idEstabelecimentoPadrao)(.+?)=(.+?)(?=;)",
+    );
+    const newCookie = headers.Cookie.replace(
+      idEstabelecimentoPattern,
+      `$1=${lojaIds[store].idEstabelecimento}`,
+    );
 
-  const parceirasResponse = await fetch(urlParceiras, {
-    method: "POST",
-    headers: {
-      ...headers,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: encodedBody,
-  });
+    const encodedBody = new URLSearchParams(requestBody);
 
-  console.log("Fim da requisição!");
-  const parceirasBody = await parceirasResponse.json();
+    const parceirasResponse = await fetch(urlParceiras, {
+      method: "POST",
+      headers: {
+        ...headers,
+        "Content-Type": "application/x-www-form-urlencoded",
+        "id-estabelecimento-autenticado": lojaIds[store].idEstabelecimento,
+        Cookie: newCookie,
+      },
+      body: encodedBody,
+    });
 
-  const table = await parceirasBody.Html;
+    console.log("Fim da requisição!");
+    const parceirasBody = await parceirasResponse.json();
 
-  const namePattern = new RegExp("(?<=<b[^>]+>).+?(?=<)", "g");
-  const parceiras = table.matchAll(namePattern);
+    const table = await parceirasBody.Html;
 
-  const idPattern = new RegExp('(?<= profissional=\").+?(?=\")', "g");
-  const ids = table.matchAll(idPattern, "g");
+    const namePattern = new RegExp("(?<=<b[^>]+>).+?(?=<)", "g");
+    const parceiras = table.matchAll(namePattern);
 
-  console.log("\nNome das parceiras:\n");
-  for (const parceira of parceiras) {
-    data["14"].nome.push(he.decode(parceira[0]));
-  }
+    const idPattern = new RegExp('(?<= profissional=\").+?(?=\")', "g");
+    const ids = table.matchAll(idPattern, "g");
 
-  console.log("Ids:\n");
-  for (const id of ids) {
-    data["14"].id.push(id[0]);
+    console.log("\nNome das parceiras:\n");
+    for (const parceira of parceiras) {
+      data[store].nome.push(he.decode(parceira[0]));
+    }
+
+    console.log("\nIds:\n");
+    for (const id of ids) {
+      data[store].id.push(id[0]);
+    }
   }
 }
 
-async function getPartnersInfo(ids) {
-  for (const id of ids) {
-    const registers = await makeRequisitionForPartner(id);
+async function getPartnersInfo(store) {
+  for (const id of data[store].id) {
+    const registers = await makeRequisitionForPartner(store, id);
     let resultado = {};
 
     registers.map((r) => {
@@ -95,15 +139,16 @@ async function getPartnersInfo(ids) {
         quantidadeVendida: r.QuantidadeVendida,
       };
     });
-    data["14"].resultados.push(resultado);
+    data[store].resultados.push(resultado);
   }
 }
 
-async function makeRequisitionForPartner(id) {
+async function makeRequisitionForPartner(store, id) {
   console.log("Pegando informação do usuário ", id);
+
   const body = JSON.stringify({
     Filtro: {
-      DataFim: finalDate, //using us date
+      DataFim: finalDate,
       DataInicio: startDate,
       VisualizarPor: 2,
       IdsServicos: [],
@@ -120,9 +165,22 @@ async function makeRequisitionForPartner(id) {
       quantidadeRegistrosAparecendo: 1,
     },
   });
+
+  const idEstabelecimentoPattern = new RegExp(
+    "(?<=idEstabelecimentoPadrao)(.+?)=(.+?)(?=;)",
+  );
+  const newCookie = headers.Cookie.replace(
+    idEstabelecimentoPattern,
+    `$1=${lojaIds[store].idEstabelecimento}`,
+  );
+
   const response = await fetch(urlServicos, {
     method: "POST",
-    headers,
+    headers: {
+      ...headers,
+      "id-estabelecimento-autenticado": lojaIds[store].idEstabelecimento,
+      Cookie: newCookie,
+    },
     body,
   });
   const responseBody = await response.json();
